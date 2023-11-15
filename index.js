bd61e8adfd0a7bd3de9ef6ebbea9dda8459ad272
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2023-11-15 12:41:00 +0300

    edited degrees

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ for(i=0;i<numberOfPetals;i++){
     petalCont.className="petalCont inner";
     petalCont.innerHTML="<div class='petal inner'></div>";
 
-    petalCont.style.transform = 'rotate('+(55+(i*(360/numberOfPetals)))+'deg)';
+    petalCont.style.transform = 'rotate('+(54+(i*(360/numberOfPetals)))+'deg)';
     document.getElementById('flower').appendChild(petalCont);
 }
 for(i=0;i<numberOfPetals;i++){
     let petalCont = document.createElement('div');
     petalCont.className="petalCont inner2";
     petalCont.innerHTML="<div class='petal inner2'></div>";
-    petalCont.style.transform = 'rotate('+(35+(i*(360/numberOfPetals)))+'deg)';
+    petalCont.style.transform = 'rotate('+(36+(i*(360/numberOfPetals)))+'deg)';
     document.getElementById('flower').appendChild(petalCont);
 }
 let code=(i*2*(180/numberOfPetals))
